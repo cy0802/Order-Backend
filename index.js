63d781163710ea6bce7becfa80d367dac3b98159
cy0802 <amyyoyo0802@gmail.com>
2024-09-27 01:28:37 +0800

    enable cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require ('express');
+const cors = require('cors');
 const productController = require('./controllers/product');
 const orderController = require('./controllers/order');
 const app = express();
 const port = 8000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
         res.send('Hello World!');
